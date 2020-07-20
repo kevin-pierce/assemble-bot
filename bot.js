4b51882f6424cd2fe52a7d4aa764275b02c0b74d
Kevin Pierce <kevinpierce08@gmail.com>
2020-07-20 12:24:03 -0400

    Resolved issue in mentioning users, added a BM message to the end of the mention for comedic effect

--- bot.js
+++ bot.js
@@ -8,9 +8,18 @@ client.on("ready", () => {
 
 // Set the global variable for the Bot's command prefix
 const PREFIX = "!";
+const bmMsgArr = ["Isaiah goes 0/10 Bard support in ranked again",
+                  "Flips decides to work on Foodprint for the 9th hour today",
+                  "Kevin has to play Minecraft with his sister",
+                  "Eric neighbours yell at him to quiet down",
+                  "Steven decides to play Sett jungle again"]
 
 // Handling for commands
 client.on("message", async message => {
+
+    // Initialize an empty message and embed
+    let botMsg = "";
+    let botEmbed = new Discord.MessageEmbed();
 
     // Do nothing if the message does not start with the command prefix, or the bot is sending the message
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
@@ -19,21 +28,27 @@ client.on("message", async message => {
     const args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
+    // The parent command
     if (command === "assemble") {
 
         // If the user does not specify the game to play
         if (!args.length) {
-            return message.channel.send("Hey <@" + message.author.id + ">, you must specify a game!");
+            botMsg = "Hey <@" + message.author.id + ">, you must specify a game!";
+            return message.channel.send(botMsg);
         }
 
+        // User specifies League
         else if (args[0] === "league" || args[0] === "lol"){
-            return message.channel.send("this worked");
+            botMsg = "<@!308071233990164480>, <@!91498883980480512>, <@604828203722473502>, <@109750986783924224>, <@!323946802598510593>, \nGet on quick before " + getBMMessage();
+            return message.channel.send(botMsg);
         }
     }
-
 })
 
-
+const getBMMessage = () => {
+    let msg = "";
+    return msg = bmMsgArr[Math.floor(Math.random() * 4)]
+}
 
 // Login to the 
 client.login(config.token)
