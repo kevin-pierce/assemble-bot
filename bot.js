9a0773ff156eb52dc0a1406fda8de3a9524e9662
Kevin Pierce <kevinpierce08@gmail.com>
2020-07-20 11:41:17 -0400

    Setup basic command syntax, incorporated debugging to assess if it works as intended

--- bot.js
+++ bot.js
@@ -6,5 +6,34 @@ client.on("ready", () => {
     console.log("Connected as " + client.user.tag);
 });
 
+// Set the global variable for the Bot's command prefix
+const PREFIX = "!";
+
+// Handling for commands
+client.on("message", async message => {
+
+    // Do nothing if the message does not start with the command prefix, or the bot is sending the message
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    // Isolate the arguments that occur after the command prefix
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if ("command" === "play") {
+
+        // If the user does not specify the game to play
+        if (!args.length) {
+            return message.channel.send("error");
+        }
+
+        else if (args[0] === "league" || args[0] === "lol"){
+            return message.channel.send("this worked");
+        }
+    }
+
+})
+
+
+
 // Login to the 
 client.login(config.token)
