fa6c9940ec38e43aa716797f2e63064f9d346e25
Kevin Pierce <kevinpierce08@gmail.com>
2020-07-22 14:03:31 -0400

    Resolving issues with Heroku

--- bot.js
+++ bot.js
@@ -5,14 +5,12 @@ const { readdirSync } = require("fs");
 
 client.commands = new Discord.Collection(); // Create a new Collection consisting of all our Discord bot commands
 
+// Place all command files into our Collection
 const commandFiles = readdirSync("./commands/").filter(file => file.endsWith(".js"));
 for (const file of commandFiles){
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
-
-let voiceChannel;
 
 client.on("ready", () => {
     voiceChannel = client.channels.fetch("325445196856688650");
@@ -21,7 +19,6 @@ client.on("ready", () => {
 
 // Handling for commands
 client.on("message", async message => {
-    let botMsg = "";
     // Do nothing if the message does not start with the command prefix, or the bot is sending the message
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
     // Isolate the arguments that occur after the command prefix
@@ -35,4 +32,4 @@ client.on("message", async message => {
 })
 
 // Login to the client
-client.login(config.token)
+client.login(config.token);
